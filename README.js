b7ec10416b550422bb2d40c45019c6f82ab1f940
XBrayen <108976157+XBrayen@users.noreply.github.com>
2022-07-10 03:13:52 +0700

    Add files via upload

---
+++ README.js
@@ -0,0 +1,15 @@
+[![Typing SVG](https://readme-typing-svg.herokuapp.com?font=Lemon+milk&color=F70000&lines=Welcome+to+my+GitHub+Pages;+Enjoy+my+content...)](https://github.com/Eskeyz)
+
+  <ul>
+  <li>🎁  <a href="https://trakteer.id/eskey">Goceng </a></li>
+  <li>🎨  <a href="https://marketplace.visualstudio.com/items?itemName=Eskeyz.eskey-theme&ssr=false#overview">VSCode Theme</a></li>
+  <li>🪐  <a href="https://eskeyz.github.io/">Website</a></li>
+</ul>
+<br>
+
+![Anurag's GitHub stats](https://github-readme-stats.vercel.app/api?username=Eskeyz&theme=midnight-purple&show_icons=true)
+
+[![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=Eskeyz&layout=compact)](https://github.com/anuraghazra/github-readme-stats)
+
+<hr>
+<p>-</p>
